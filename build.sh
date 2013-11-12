2ac083081c53514007ed29e8609a41a74ba7eec5
Takuya ASADA <syuu@dokukino.com>
2013-11-12 04:57:03 -0800

    fix libuv version, use osv official branch

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 rm -rf libuv
 git clone https://github.com/joyent/libuv.git
+git checkout v0.10.19
 cd libuv
 ./gyp_uv.py -f make
 make -C out
 sudo cp out/Debug/libuv.a /usr/lib
 sudo cp include/uv* /usr/include
 cd -
-rm -rf osv
-git clone https://github.com/syuu1228/osv.git
+sudo rm -rf osv
+git clone https://github.com/cloudius-systems/osv.git
 cd osv
 git checkout module
 git submodule update --init
