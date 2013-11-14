6243566b0e62ec750a7f4bf774d13f9ca9b510dd
Takuya ASADA <syuu@dokukino.com>
2013-11-13 16:36:56 -0800

    shell.rb moved to mruby-eshell repository

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ $# -gt 0 ]; then
 	MEM="-m $1"
 fi
 cd osv/
-sudo ./scripts/run.py $MEM -n -v -i build/release/loader.img -e "--nomount mruby.so shell.rb"
+sudo ./scripts/run.py $MEM -n -v -i build/release/loader.img -e "--nomount mruby.so eshell.rb"
