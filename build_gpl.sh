2e2cbb493de48532e9b6e3ccf120651133b141d1
Takuya ASADA <syuu@dokukino.com>
2013-11-16 22:54:27 -0800

    add GPL version

---
+++ build_gpl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+sudo rm -rf osv
+git clone https://github.com/cloudius-systems/osv.git
+cd osv
+git submodule init
+git submodule update
+cp ../osv_hack/config_gpl.json .
+cp ../osv_hack/*.skel .
+cp ../osv_hack/build.mk .
+cp ../osv_hack/Makefile .
+sudo make external all $*
+cd -
